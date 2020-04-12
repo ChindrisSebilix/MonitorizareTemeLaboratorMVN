848d325bb1d66118ab7bb0e71cecdb6d65905ff3
unknown <sebichindris@gmail.com>
2020-04-12 17:00:13 +0300

    Lab2 Tests

--- src/test/java/org/example/TestAddStudent.java
+++ src/test/java/org/example/TestAddStudent.java
@@ -6,8 +6,12 @@ import org.example.domain.Tema;
 import org.example.repository.*;
 import org.example.service.Service;
 import org.example.validation.*;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class TestAddStudent {
 
@@ -30,29 +34,96 @@ public class TestAddStudent {
         this.temaRepo = new TemaXMLRepository(temaValidator, "teme.xml");
         this.noteRepo = new NotaXMLRepository(notaValidator, "note.xml");
 
-        this.service = new Service(studRepo, temaRepo, noteRepo); }
+        this.service = new Service(studRepo, temaRepo, noteRepo);
+    }
 
     @Before
-    public void setup() { this.service = new Service(studRepo, temaRepo, noteRepo); }
+    public void setup() {
+        this.service = new Service(studRepo, temaRepo, noteRepo);
+    }
 
     @Test
-    public void testSuccesAddStudent(){
+    public void testSuccesAddStudent() {
 
         try {
             int ret = service.saveStudent("5", "succesful", 200);
-            assert ret==0;
+            assert ret == 1;
             int ret2 = service.saveStudent("6", "succesful2", 300);
-            assert ret2==0;
+            assert ret2 == 1;
+        } catch (ValidationException vex) {
+            assert false;
         }
-        catch (ValidationException vex)
-        {
+
+        //BVA
+        try {
+            service.saveStudent("20", "success", 111);
+            assert true;
+        } catch (ValidationException e) {
+            assert false;
+        }
+        try {
+            service.saveStudent("21", "fail", 937);
+            assert true;
+        } catch (ValidationException e) {
             assert false;
         }
     }
 
-    /*
-    @Test
-    public void testFailAddStudent(){
 
-    }*/
+    @Test
+    public void testFailAddStudent() {
+
+        try {
+            int ret = service.saveStudent("11", "", 200);
+        } catch (ValidationException e) {
+            assert true;
+        }
+        try {
+            service.saveStudent("12", null, 300);
+            assert false;
+        } catch (ValidationException e) {
+            assert true;
+
+        }
+//        try {
+//            service.saveStudent("13", "fail", 300.0);
+//            assert false;
+//        } catch (Exception e) {
+//            assert true;
+//        }
+        try {
+            service.saveStudent("14", "fail", 110);
+            assert false;
+        } catch (ValidationException e) {
+            assert true;
+        }
+
+
+        try {
+            service.saveStudent("16", "fail", 938);
+            assert false;
+        } catch (ValidationException e) {
+            assert true;
+        }
+
+        try {
+            service.saveStudent("17", "fail", -2);
+            assert false;
+        } catch (ValidationException e) {
+            assert true;
+        }
+
+
+    }
+
+//    @After
+//        public void tearDown() {
+//            Iterator<Student> it = studRepo.findAll().iterator();
+//            while (it.hasNext()) {
+//                Student aux = it.next();
+//                it.remove();
+//                it = aux;
+//            }
+//
+//    }
 }
